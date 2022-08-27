5218c298492c5f3e8a118a83590ab5198f1c2890
devvagadiya <88654563+devvagadiya@users.noreply.github.com>
2022-08-27 15:39:22 +0530

    create user crud and add generateFilterQuery

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -2,19 +2,42 @@ import jwt from "jsonwebtoken";
 import { UserInputError } from "apollo-server-express";
 import { combineResolvers } from 'graphql-resolvers';
 import { isAdmin, isAuthenticated } from "./auth";
+import { FilterQuery } from './../functions/generateFilterQuery';
 const generateToken = async (user, expiresIn) => {
   const { id, email } = user;
   const token = await jwt?.sign({ id, email }, process.env.SECRET, {
     expiresIn,
   });
-  console.log("token", token);
   return token;
 };
 
 export default {
+  Query: {
+		// getAllEducation with paginate
+		getAllUser: combineResolvers(isAuthenticated, (parent, args, { models }) => {
+			return new Promise((resolve, reject) => {
+				const filterText = FilterQuery(args?.search, "userTbl");
+				const sort = { [args?.sort?.key]: args?.sort?.type };
+				const option = { page: args?.page, limit: args?.limit, sort };
+				models?.User?.paginate({ ...filterText, isDeleted: false }, option, (err, results) => {
+					if (err) reject(err);
+					resolve({ count: results?.total || 0, data: results?.docs || [] });
+				});
+			});
+		}),
+
+		// getAllEducation without paginate
+		getUser: combineResolvers(isAuthenticated, (parent, args, { models }) => {
+			return new Promise((resolve, reject) => {
+				models?.User?.find({ isDeleted: false }).exec((err, res) => {
+					if (err) reject(err);
+					else resolve(res);
+				});
+			});
+		}),
+	},
   Mutation: {
     signIn: async (_, { email, password }, { models }) => {
-      console.log("it call");
       const user = await models.User.findOne({ email });
       if (!user) {
         throw new UserInputError("Email not found.");
@@ -31,7 +54,7 @@ export default {
 
     createUser:combineResolvers(isAuthenticated,(_,{input},{models,me})=>{
         return new Promise((resolve,reject)=>{
-            models?.User.findOne({email:input?.email}).exec((err,res)=>{
+            models?.User.findOne({email:input?.email,isDeleted:false}).exec((err,res)=>{
                 if (err) reject(err);
 					else if (res) reject("This email is already exits.");
                     else{
@@ -44,6 +67,25 @@ export default {
                     }
             })
         })
+    }),
+
+    updateUser:combineResolvers((_,{input},{models,me})=>{
+      return new Promise((resolve,reject)=>{
+        models?.User?.findOneAndUpdate({_id:me?.id,isDeleted:false},input,{new:true},(err,res)=>{
+          if(err)reject(err)
+          else resolve(res)
+        })
+      })
+    }),
+    deleteUser:combineResolvers((_,{id},{models})=>{
+      return new Promise((resolve,reject)=>{
+        models?.User?.findOneAndUpdate({_id:id,isDeleted:false},{isDeleted:true},{new:true},(err,res)=>{
+          if(err)reject(err)
+          else if(!res)resolve("This user not found.")
+          else resolve(true)
+        })
+      })
     })
+
   },
 };
