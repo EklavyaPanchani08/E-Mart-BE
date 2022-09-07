1fe6034956577e7da92ea4986927c1cab443cf9d
devvagadiya <88654563+devvagadiya@users.noreply.github.com>
2022-09-07 20:02:36 +0530

    code clean

--- src/schema/user.js
+++ src/schema/user.js
@@ -1,61 +1,66 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
-  type Token {
-    token: String
-    user: User
-  }
-  type User {
-    id: ID
-    firstName: String
-    lastName: String
-    email: String
-    password: String
-    gender: String
-    isActive: Boolean
-    isVerified: Boolean
-    isApproved: Boolean
-    createdBy: ID
-    updatedBy: ID
-    createdAt: Date
-    updatedAt: Date
-  }
-  type UserRes {
-    id: ID
-    firstName: String
-    lastName: String
-    email: String
-    gender: String
-    isActive: Boolean
-    isVerified: Boolean
-    isApproved: Boolean
-    createdBy: ID
-    updatedBy: ID
-    createdAt: Date
-    updatedAt: Date
-  }
-  type UserPaginate {
-		count: Int
-		data: [User]
-	}
+     type Token {
+          token: String
+          user: User
+     }
+     type User {
+          id: ID
+          firstName: String
+          lastName: String
+          email: String
+          password: String
+          gender: String
+          isActive: Boolean
+          isVerified: Boolean
+          isApproved: Boolean
+          createdBy: ID
+          updatedBy: ID
+          createdAt: Date
+          updatedAt: Date
+     }
+     type UserRes {
+          id: ID
+          firstName: String
+          lastName: String
+          email: String
+          gender: String
+          isActive: Boolean
+          isVerified: Boolean
+          isApproved: Boolean
+          createdBy: ID
+          updatedBy: ID
+          createdAt: Date
+          updatedAt: Date
+     }
+     type UserPaginate {
+          count: Int
+          data: [User]
+     }
 
-  input UserInput {
-    firstName: String
-    lastName: String
-    email: String
-    password: String
-    gender: String
-  }
+     input UserInput {
+          firstName: String
+          lastName: String
+          email: String
+          password: String
+          gender: String
+     }
 
-  extend type Query {
-		getAllUser(page: Int, limit: Int, sort: Sort, search: String): UserPaginate
-		getUser: [User]
-	}
-  
-  extend type Mutation {
-    createUser(input: UserInput): User
-    signIn(email: String, password: String): Token
-    updateUser(input:UserInput):User
-    deleteUser(id:ID):Boolean
-  }
+     extend type Query {
+          getAllUser(
+               page: Int
+               limit: Int
+               sort: Sort
+               search: String
+          ): UserPaginate
+          getUser: [User]
+     }
+
+     extend type Mutation {
+          createUser(input: UserInput): User
+          signIn(email: String, password: String): Token
+          updateUser(input: UserInput): User
+          deleteUser(id: ID): Boolean
+     }
 `;
